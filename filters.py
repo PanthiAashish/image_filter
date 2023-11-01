71e53d9bf79b66c5ff0e7f2aadb169e28c049d37
Aashish Panthi <panthiaashish84@gmail.com>
2023-11-01 00:09:24 -0500

    added red stripes option

--- filters.py
+++ filters.py
@@ -12,3 +12,20 @@ def darken(pixels):
       b = new_pixels[row][col][2]
       new_pixels[row][col] = [r / 2, g / 2, b / 2]
   return new_pixels
+
+
+def red_stripes(pixels):
+  new_pixels = copy.deepcopy(pixels)
+
+  for row in range(len(pixels)):
+    for col in range(len(pixels[row])):
+      r = new_pixels[row][col][0]
+      g = new_pixels[row][col][1]
+      b = new_pixels[row][col][2]
+      
+      if col % 100 < 50:
+        new_pixels[row][col] = [255, 0, 0]
+      else:
+        new_pixels[row][col] = [r, g, b]
+      
+  return new_pixels
