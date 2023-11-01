5fd1d81319ffa62334b8430e179ad3149d84ece6
Aashish Panthi <panthiaashish84@gmail.com>
2023-11-01 00:08:00 -0500

    added the darkening option

---
+++ filters.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from image_converter import list_to_image, image_to_list
+import copy
+
+# An example function provided for you that darkens an image.
+def darken(pixels):
+  new_pixels = copy.deepcopy(pixels)
+  for row in range(len(pixels)):
+    for col in range(len(pixels[row])):
+      r = new_pixels[row][col][0]
+      g = new_pixels[row][col][1]
+      b = new_pixels[row][col][2]
+      new_pixels[row][col] = [r / 2, g / 2, b / 2]
+  return new_pixels
